e5bfed02ef9088b9e62b64f9a774d850dfa339ae
mukeshc93 <mukeshc.93@outlook.com>
2016-07-23 13:23:08 +0530

    Delete insurance.R

--- insurance.R
+++
@@ -1,9 +0,0 @@
-insurance=read.csv(file.choose(),stringsAsFactors = T, header = T)
-str(insurance)
-summary(insurance)
-hist(insurance$charges)
-cor(insurance[c("age","bmi","children","charges")])
-model=lm(charges~age+bmi+children+smoker+region,data=insurance)
-insurance_test=insurance[1200:1338,]
-test=predict(model,insurance_test)
-summary(model)
